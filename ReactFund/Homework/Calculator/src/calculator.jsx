179ba3b5a0d4124750ed752bc34c71e93da230ac
Garrett <gnjulaka@gmail.com>
2021-02-23 21:11:25 -0600

    Input setup. Need to make its setting state correct.

--- ReactFund/Homework/Calculator/src/calculator.jsx
+++ ReactFund/Homework/Calculator/src/calculator.jsx
@@ -4,14 +4,34 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     // your code here
+
+    this.state = { result: 0, num1: "", num2: ""};
+    this.setNum1 = this.setNum1.bind(this);
   }
 
   // your code here
 
+  setNum1(e) {
+
+    e.preventDefault();
+    alert("fired");
+
+  }
+
+  setNum2(e) {
+
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1> // replace this with your code
+        <label>Num1</label>
+        <input onChange={this.setNum1} value={this.state.num1}></input>
+        <br />
+        <label>Num2</label>
+        <input></input>
+        <h1>Result</h1>
+        <h1>{this.state.result}</h1>
       </div>
     );
   }
