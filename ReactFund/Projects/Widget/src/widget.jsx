c584e7e8762372effbc92d271dca6ce34df2bdf9
Garrett <gnjulaka@gmail.com>
2021-02-25 15:11:13 -0600

    You can only react dom render once.

--- ReactFund/Projects/Widget/src/widget.jsx
+++ ReactFund/Projects/Widget/src/widget.jsx
@@ -3,8 +3,14 @@ console.log("Webpack");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from "./clock.jsx"
+import Tab from "./tab.jsx"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<Clock />, root);
+  ReactDOM.render(
+    <div>
+      <Clock />
+      <Tab />
+    </div>, root);
+  
 });
