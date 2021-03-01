1ad0fc99a769170bdb139eb27703a4be32ce4458
Garrett <gnjulaka@gmail.com>
2021-03-01 16:35:10 -0600

    Added to much to one method. Let did mount chaneg the state. Work off the changed state.

--- ReactFund/Projects/Widget/src/weather.jsx
+++ ReactFund/Projects/Widget/src/weather.jsx
@@ -5,31 +5,37 @@ class Weather extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { time: new Date() }
+        this.state = { lat: "", long: "" }
 
         this.succ = this.succ.bind(this);
+        this.getLoc = this.getLoc.bind(this);
+    
     }
 
     componentDidMount() {
-        console.log("Weather Mount Fired");
+        navigator.geolocation.getCurrentPosition(this.getLoc);
+    }
 
-        navigator.geolocation.getCurrentPosition(this.succ);
-
+    getLoc(pos) {
+        this.setState( {lat: pos.coords.latitude, long: pos.coords.longitude} )
     }
 
     succ(pos) {
         console.log(pos.coords.latitude);
         console.log(pos.coords.longitude);
 
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API.weather}`
+        let html = "";
+        let url = `http://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${API.weather}&units=imperial`
         let xhr = new XMLHttpRequest();
         xhr.open("GET", url);
 
         xhr.send();
 
         xhr.onload = function() {
-            console.log(xhr.response);
+            html = xhr.response;
         }
+
+        return <div>html</div>
     }
 
     render() {
@@ -38,7 +44,8 @@ class Weather extends React.Component {
             <div className="weather">
                 <h1 className="header">Weather</h1>
                 <div className="weather-box">
-
+                    here
+                    {this.componentDidMount()}
                 </div>
             </div>
         )
