812954fc352a0dda06eec2f233640c2b4c176220
Garrett <gnjulaka@gmail.com>
2021-03-12 13:35:02 -0600

    ToDo setup complete

---
+++ ReduxFund/Projects/ToDo/frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<div>Hooked</div>, document.getElementById('root'));
+});
