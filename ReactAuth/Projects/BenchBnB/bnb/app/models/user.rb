af4909ef2b28c2e8b0659eb8b8ce869740bd2bf2
Garrett <gnjulaka@gmail.com>
2021-04-24 21:22:19 -0500

    Session token isnt firing for some reason.

--- ReactAuth/Projects/BenchBnB/bnb/app/models/user.rb
+++ ReactAuth/Projects/BenchBnB/bnb/app/models/user.rb
@@ -6,11 +6,55 @@ class User < ApplicationRecord
     validates :password_digest, presence: { message: "Password can't be blank" }
     validates :password, length: { minimum: 6, allow_nil: true }
 
+    before_validation(on: :create) do
+        :ensure_session_token
+    end
+  
 
+    after_initialize do |user|
+        ensure_session_token
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user.nil?
+            return nil
+        end
+
+        if user.is_password?(password)
+            return user
+        else
+             return nil
+        end
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def fired?
+        puts "Anus"
+    end
+    
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+    end
 
     def password=(password)
         @password = password
         self.password_digest = BCrypt::Password.create(password)
     end
+
+    def is_password?(password)
+        check = BCrypt::Password.new(password_digest)
+        check.is_password?(password)
+    end
+    def ensure_session_token
+        self.session_token || User.generate_session_token
+    end
+
 
 end
--- ReactAuth/Projects/BenchBnB/bnb/frontend/bench_bnb.jsx
+++ ReactAuth/Projects/BenchBnB/bnb/frontend/bench_bnb.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import * as userUtil from '../frontend/util/util_users' 
+
+window.makeUser = userUtil.createUser
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
